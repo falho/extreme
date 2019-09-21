322342443e1afcc2122dab30b74b099182bebd77
Benedek Kiss <kisspbenedek@gmail.com>
2019-09-22 01:10:57 +0200

    Extreme digital - add basic html of  header and sidebar

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// thanks: https://medium.com/dailyjs/building-a-react-component-with-webpack-publish-to-npm-deploy-to-github-guide-6927f60b3220
+
+import React from 'react';
+import './styles.css';
+const MyComponent = () => (
+    <h1>Hello from My Component</h1>
+);
+export default MyComponent;
